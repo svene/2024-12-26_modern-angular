7ff1f8cd979cbd773feb0d6adfd099b730de4cc3
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-12-30 10:10:43 +0100

    using BehaviorSubject for buttons of app as well

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import {Component, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {D01SelectParentComponent} from './d01-select/d01-select-parent.component';
 import {D02NameParentComponent} from './d02-name/d02-name-parent.component';
 import {NgClass} from '@angular/common';
 import {D03SelectParentComponent} from './d03-select/d03-select-parent.component';
+import {BehaviorSubject} from 'rxjs';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-root',
@@ -13,10 +15,8 @@ import {D03SelectParentComponent} from './d03-select/d03-select-parent.component
 })
 export class AppComponent {
   title = 'modern-angular';
-  selectedDemo = signal<Demo>('d03-select');
-
-  onClick(demo: Demo) {
-    this.selectedDemo.set(demo);
-  }
+  btnClick$$ = new BehaviorSubject<Demo>('d01-select');
+  btnClick = toSignal<Demo>(this.btnClick$$)
+  selectedDemo = computed(() => this.btnClick);
 }
 export type Demo = 'd01-select' | 'd02-name' | 'd03-select';
