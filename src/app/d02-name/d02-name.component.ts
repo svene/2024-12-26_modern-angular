c5dda4f3a6baddb0bc877bfe5621595107089a23
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-12-30 07:23:34 +0100

    using 'linkedSignal' for second demo

--- src/app/d02-name/d02-name.component.ts
+++ src/app/d02-name/d02-name.component.ts
@@ -1,4 +1,4 @@
-import {Component, computed, input, signal} from '@angular/core';
+import {Component, input, linkedSignal} from '@angular/core';
 
 /**
  * See https://medium.com/@amosisaila/learn-when-to-use-signal-effects-in-angular-and-why-you-should-avoid-overusing-them-a0d6516032c1
@@ -17,11 +17,10 @@ export class D02NameComponent {
   // use the following pattern if 'childName' should be changeable both from outside and from inside but
   // a change from inside is not reflected to the outside:
   name = input('');
-  childName_ = computed(() => signal(this.name())); // call set() on this to change value from inside. Initial value is take from input
-  childName = computed(() => this.childName_()()); // use this for reading in template (bc. double ()() in template is ugly)
+  childName = linkedSignal(() => this.name());
 
   setChildName(v: string): void {
-    this.childName_().set(v);
+    this.childName.set(v);
   }
 
 }
