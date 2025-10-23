b82bf2fdccae6b995e66db0473b2d4cf76357791
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-10-23 14:46:15 +0200

    signal-forms: flight booking: first step of errorhandling

--- src/app/d08-sigform3/d08-sigform3.component.ts
+++ src/app/d08-sigform3/d08-sigform3.component.ts
@@ -1,13 +1,15 @@
 import {Component, inject, linkedSignal} from '@angular/core';
 import {FlightDetailStore} from './flight-detail.store';
 import {Control, form, required} from '@angular/forms/signals';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-d08-sigform3',
   templateUrl: './d08-sigform3.component.html',
   styleUrls: ['./d08-sigform3.component.scss'],
   imports: [
-    Control
+    Control,
+    JsonPipe
   ]
 })
 export class D08Sigform3Component {
