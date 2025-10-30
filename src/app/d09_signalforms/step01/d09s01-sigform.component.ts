e88701848e9ee65bf4b905565dc4f4d1366e2921
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-10-30 06:15:13 +0100

    flight-signal-forms: step 1: field 'id'

--- src/app/d09_signalforms/step01/d09s01-sigform.component.ts
+++ src/app/d09_signalforms/step01/d09s01-sigform.component.ts
@@ -1,14 +1,24 @@
-import {Component} from '@angular/core';
+import {Component, inject, linkedSignal} from '@angular/core';
+import {FlightDetailStore} from './flight-detail.store';
+import {Field, form, required} from '@angular/forms/signals';
 
 @Component({
   selector: 'app-d09s01-sigform',
-  template: `
-    <p>hello</p>
-  `,
+  templateUrl: 'd09s01-sigform.component.html',
   imports: [
+    Field
   ]
 })
 export class D09s01SigformComponent {
 
+  private store = inject(FlightDetailStore);
+
+  flight = linkedSignal(() => this.store.flight());
+
+  flightForm = form(this.flight, (path) => {
+    required(path.id)
+    required(path.from)
+    required(path.to)
+  });
 
 }
