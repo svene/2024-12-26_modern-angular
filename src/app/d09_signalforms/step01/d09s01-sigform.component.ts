d6c469ea5b36d5e2567aec4b7c05c3b37007f8ae
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-10-30 06:44:32 +0100

    flight-signal-forms: step 1: revert 'more errorhandling'

--- src/app/d09_signalforms/step01/d09s01-sigform.component.ts
+++ src/app/d09_signalforms/step01/d09s01-sigform.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject, linkedSignal} from '@angular/core';
 import {FlightDetailStore} from './flight-detail.store';
-import {customError, Field, form, minLength, required, validate} from '@angular/forms/signals';
+import {Field, form, required} from '@angular/forms/signals';
 import {JsonPipe} from '@angular/common';
 
 @Component({
@@ -18,25 +18,9 @@ export class D09s01SigformComponent {
   flight = linkedSignal(() => this.store.flight());
 
   flightForm = form(this.flight, (path) => {
-    required(path.id, { message: 'Please enter a value!' });
-    required(path.from, { message: 'Please enter a value!' });
-    required(path.to, { message: 'Please enter a value!' });
-
-    minLength(path.from, 3);
-
-    const allowed = ['Graz', 'Hamburg', 'Zürich'];
-    validate(path.from, (ctx) => {
-      const value = ctx.value();
-      if (allowed.includes(value)) {
-        return null;
-      }
-
-      return customError({
-        kind: 'city',
-        value,
-        allowed,
-      });
-    });
+    required(path.id)
+    required(path.from)
+    required(path.to)
   });
 
 }
