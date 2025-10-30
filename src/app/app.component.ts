c6b376d54bf9be057b2a52e80de1e848cc7eaa7f
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-10-30 05:30:37 +0100

    flight-signal-forms: step 1: setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,29 @@ import {D05Http1Component} from './d05-http1/d05-http1.component';
 import {D06Sigform1Component} from './d06-sigform1/d06-sigform1.component';
 import {D07Sigform2Component} from './d07-sigform2/d07-sigform2.component';
 import {D08Sigform3Component} from './d08-sigform3/d08-sigform3.component';
+import {D09s01SigformComponent} from './d09_signalforms/step01/d09s01-sigform.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgClass, D01SelectParentComponent, D02NameParentComponent, D03SelectParentComponent, D04NameParentComponent, D05Http1Component, D06Sigform1Component, D07Sigform2Component, D08Sigform3Component],
+  imports: [
+    RouterOutlet,
+    NgClass,
+    D01SelectParentComponent,
+    D02NameParentComponent,
+    D03SelectParentComponent,
+    D04NameParentComponent,
+    D05Http1Component,
+    D06Sigform1Component,
+    D07Sigform2Component,
+    D08Sigform3Component,
+    D09s01SigformComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'modern-angular';
-  selection = signal<Demo>('d08-sigform3');
+  selection = signal<Demo>('d09s01-sigform');
 
   selectDemo(v: Demo) {
     this.selection.set(v);
@@ -29,4 +42,5 @@ export type Demo =
   'd01-select'
   | 'd02-name' | 'd03-select' | 'd04-name' | 'd05-http1'
   | 'd06-sigform1' | 'd07-sigform2' | 'd08-sigform3'
+  | 'd09s01-sigform'
   ;
---
+++ src/app/d09_signalforms/step01/d09s01-sigform.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-d09s01-sigform',
+  template: `
+    <p>hello</p>
+  `,
+  imports: [
+  ]
+})
+export class D09s01SigformComponent {
+
+
+}
