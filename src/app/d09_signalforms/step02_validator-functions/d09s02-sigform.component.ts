0ed56ce11fda3bdb0509afe9bf2eedf8bdfc5c5e
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-11-01 21:06:59 +0100

    refactoring: same file naming to make directory diffs possible

--- src/app/d09_signalforms/step02_validator-functions/d09s02-sigform.component.ts
+++ src/app/d09_signalforms/step02_validator-functions/d09s02-sigform.component.ts
@@ -1,47 +1,14 @@
-import {Component, inject, linkedSignal} from '@angular/core';
-import {FlightDetailStore} from './flight-detail.store';
-import {customError, Field, FieldPath, form, minLength, required, validate} from '@angular/forms/signals';
-import {JsonPipe, NgClass} from '@angular/common';
+import {Component} from '@angular/core';
+import {SigformComponent} from './sigform.component';
 
 @Component({
   selector: 'app-d09s02-sigform',
-  templateUrl: 'd09s02-sigform.component.html',
+  template: `
+    <app-sigform></app-sigform>
+  `,
   imports: [
-    Field,
-    JsonPipe,
-    NgClass
+    SigformComponent
   ]
 })
 export class D09s02SigformComponent {
-
-  private store = inject(FlightDetailStore);
-
-  flight = linkedSignal(() => this.store.flight());
-
-  validateCity(path: FieldPath<string>, allowed: string[]): void {
-    validate(path, (ctx) => {
-      const value = ctx.value();
-      if (allowed.includes(value)) {
-        return null;
-      }
-
-      return customError({
-        kind: 'city',
-        value,
-        allowed,
-      });
-    });
-  }
-
-  flightForm = form(this.flight, (path) => {
-    required(path.id, { message: 'Please enter a value!' });
-    required(path.from, { message: 'Please enter a value!' });
-    required(path.to, { message: 'Please enter a value!' });
-
-    minLength(path.from, 3);
-
-    const allowed = ['Graz', 'Hamburg', 'Zürich'];
-    this.validateCity(path.from, allowed);
-  });
-
 }
