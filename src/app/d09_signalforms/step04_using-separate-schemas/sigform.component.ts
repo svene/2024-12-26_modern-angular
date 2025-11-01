3853cb726ddff012a2e8a158c3c7168de5d7de02
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-11-01 21:01:33 +0100

    refactoring: same file naming to make directory diffs possible

---
+++ src/app/d09_signalforms/step04_using-separate-schemas/sigform.component.ts
@@ -0,0 +1,25 @@
+import {Component, inject, linkedSignal} from '@angular/core';
+import {FlightDetailStore} from './flight-detail.store';
+import {Field, form} from '@angular/forms/signals';
+import {NgClass} from '@angular/common';
+import {flightSchema} from './sigform.validation';
+import {ValidationErrorsComponent} from './common/validation-errors.component';
+
+@Component({
+  selector: 'app-sigform',
+  templateUrl: 'sigform.component.html',
+  imports: [
+    Field,
+    NgClass,
+    ValidationErrorsComponent
+  ]
+})
+export class SigformComponent {
+
+  private store = inject(FlightDetailStore);
+
+  flight = linkedSignal(() => this.store.flight());
+
+  flightForm = form(this.flight, flightSchema);
+
+}
