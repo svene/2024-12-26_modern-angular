28216ac062d42344136ab91c240b9d3b31d12d5f
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2026-04-03 15:02:09 +0200

    finished: replace manual routing mechanism with angular routing

--- src/app/a-page/menu.component.ts
+++ src/app/a-page/menu.component.ts
@@ -17,42 +17,34 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
       <ul class="menu-list">
         <a routerLink="/d01-select" routerLinkActive="is-active">Select</a>
         <a routerLink="/d02-name" routerLinkActive="is-active">Name</a>
-<!--
-        <li (click)="selectDemo('d01-select')"><a [ngClass]="{'is-active': selection() === 'd01-select'}">Select</a></li>
-        <li (click)="selectDemo('d02-name')"><a [ngClass]="{'is-active': selection() === 'd02-name'}">Name</a></li>
-        <li (click)="selectDemo('d03-select')"><a [ngClass]="{'is-active': selection() === 'd03-select'}">Select 2</a></li>
-        <li (click)="selectDemo('d04-name')"><a [ngClass]="{'is-active': selection() === 'd04-name'}">Name 2</a></li>
--->
+        <a routerLink="/d03-select" routerLinkActive="is-active">Select 2</a>
+        <a routerLink="/d04-name" routerLinkActive="is-active">Name 2</a>
       </ul>
       <p class="menu-label">HTTP</p>
       <ul class="menu-list">
-<!--        <li (click)="selectDemo('d05-http1')"><a [ngClass]="{'is-active': selection() === 'd05-http1'}">HTTP 1</a></li>-->
+        <a routerLink="/d05-http1" routerLinkActive="is-active">HTTP 1</a>
       </ul>
       <p class="menu-label">Signal Forms</p>
       <ul class="menu-list">
-<!--
-        <li (click)="selectDemo('d06-sigform1')"><a [ngClass]="{'is-active': selection() === 'd06-sigform1'}">Simple Validation</a></li>
-        <li (click)="selectDemo('d07-sigform2')"><a [ngClass]="{'is-active': selection() === 'd07-sigform2'}">Multifield Validation</a></li>
-        <li (click)="selectDemo('d08-sigform3')"><a [ngClass]="{'is-active': selection() === 'd08-sigform3'}">Flight Booking</a></li>
--->
+        <a routerLink="/d06-sigform1" routerLinkActive="is-active">Simple Validation</a>
+        <a routerLink="/d07-sigform2" routerLinkActive="is-active">Multifield Validation</a>
+        <a routerLink="/d08-sigform3" routerLinkActive="is-active">Flight Booking</a>
       </ul>
       <p class="menu-label">Signal Forms: Flight</p>
       <ul class="menu-list">
-<!--
-        <li (click)="selectDemo('d09s01-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s01-sigform'}">1: Start</a></li>
-        <li (click)="selectDemo('d09s02-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s02-sigform'}">2: Validator Functions</a></li>
-        <li (click)="selectDemo('d09s03-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s03-sigform'}">3: Show Validation Errors</a></li>
-        <li (click)="selectDemo('d09s04-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s04-sigform'}">4: Use Separate Schemas</a></li>
-        <li (click)="selectDemo('d09s05-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s05-sigform'}">5: Conditional Validation</a></li>
-        <li (click)="selectDemo('d09s06-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s06-sigform'}">6: Multifield Validation</a></li>
-        <li (click)="selectDemo('d09s07-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s07-sigform'}">7: Async Validation</a></li>
-        <li (click)="selectDemo('d09s08-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s08-sigform'}">8: Async Validation HTTP</a></li>
-        <li (click)="selectDemo('d09s09-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s09-sigform'}">9: Nested Forms</a></li>
-        <li (click)="selectDemo('d09s10-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s10-sigform'}">10: Nested Forms and Form Arrays</a></li>
-        <li (click)="selectDemo('d09s11-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s11-sigform'}">11: Subforms</a></li>
-        <li (click)="selectDemo('d09s12-sigform')"><a [ngClass]="{'is-active': selection() === 'd09s12-sigform'}">12: Custom Fields</a></li>
-        <li (click)="selectDemo('d10s01-sigform')"><a [ngClass]="{'is-active': selection() === 'd10s01-sigform'}">13: Dynamic Forms</a></li>
--->
+        <a routerLink="/d09s01-sigform" routerLinkActive="is-active">1: Start</a>
+        <a routerLink="/d09s02-sigform" routerLinkActive="is-active">2: Validator Functions</a>
+        <a routerLink="/d09s03-sigform" routerLinkActive="is-active">3: Show Validation Errors</a>
+        <a routerLink="/d09s04-sigform" routerLinkActive="is-active">4: Use Separate Schemas</a>
+        <a routerLink="/d09s05-sigform" routerLinkActive="is-active">5: Conditional Validation</a>
+        <a routerLink="/d09s06-sigform" routerLinkActive="is-active">6: Multifield Validation</a>
+        <a routerLink="/d09s07-sigform" routerLinkActive="is-active">7: Async Validation</a>
+        <a routerLink="/d09s08-sigform" routerLinkActive="is-active">8: Async Validation HTTP</a>
+        <a routerLink="/d09s09-sigform" routerLinkActive="is-active">9: Nested Forms</a>
+        <a routerLink="/d09s10-sigform" routerLinkActive="is-active">10: Nested Forms and Form Arrays</a>
+        <a routerLink="/d09s11-sigform" routerLinkActive="is-active">11: Subforms</a>
+        <a routerLink="/d09s12-sigform" routerLinkActive="is-active">12: Custom Fields</a>
+        <a routerLink="/d10s01-sigform" routerLinkActive="is-active">13: Dynamic Forms</a>
       </ul>
     </aside>
   `,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,51 @@
 import { Routes } from '@angular/router';
 import {D01SelectParentComponent} from './d01-select/d01-select-parent.component';
 import {D02NameParentComponent} from './d02-name/d02-name-parent.component';
+import {D03SelectParentComponent} from './d03-select/d03-select-parent.component';
+import {D04NameParentComponent} from './d04-name/d04-name-parent.component';
+import {D05Http1Component} from './d05-http1/d05-http1.component';
+import {D06Sigform1Component} from './d06-sigform1/d06-sigform1.component';
+import {D07Sigform2Component} from './d07-sigform2/d07-sigform2.component';
+import {D08Sigform3Component} from './d08-sigform3/d08-sigform3.component';
+import {D09s01SigformComponent} from './d09_signalforms/step01/d09s01-sigform.component';
+import {D09s02SigformComponent} from './d09_signalforms/step02_validator-functions/d09s02-sigform.component';
+import {D09s03SigformComponent} from './d09_signalforms/step03_show-validation-errors/d09s03-sigform.component';
+import {D09s04SigformComponent} from './d09_signalforms/step04_using-separate-schemas/d09s04-sigform.component';
+import {D09s05SigformComponent} from './d09_signalforms/step05_conditional-validation/d09s05-sigform.component';
+import {D09s06SigformComponent} from './d09_signalforms/step06_multifield-validation/d09s06-sigform.component';
+import {D09s07SigformComponent} from './d09_signalforms/step07_async-validation/d09s07-sigform.component';
+import {D09s08SigformComponent} from './d09_signalforms/step08_async-validation/d09s08-sigform.component';
+import {D09s09SigformComponent} from './d09_signalforms/step09_nested-forms_aircraft-and-registration/d09s09-sigform.component';
+import {D09s10SigformComponent} from './d09_signalforms/step10_nested_forms_and_form_arrays/d09s10-sigform.component';
+import {D09s11SigformComponent} from './d09_signalforms/step11_subforms/d09s11-sigform.component';
+import {D09s12SigformComponent} from './d09_signalforms/step12_custom-fields/d09s12-sigform.component';
+import {D10s01SigformComponent} from './d10_signalforms-dynamic/d10s01-sigform.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'd01-select', pathMatch: 'full'},
   {path: 'd01-select', component: D01SelectParentComponent},
   {path: 'd02-name', component: D02NameParentComponent},
+  {path: 'd03-select', component: D03SelectParentComponent},
+  {path: 'd04-name', component: D04NameParentComponent},
+
+  {path: 'd05-http1', component: D05Http1Component},
+
+  {path: 'd06-sigform1', component: D06Sigform1Component},
+  {path: 'd07-sigform2', component: D07Sigform2Component},
+  {path: 'd08-sigform3', component: D08Sigform3Component},
+
+  {path: 'd09s01-sigform', component: D09s01SigformComponent},
+  {path: 'd09s02-sigform', component: D09s02SigformComponent},
+  {path: 'd09s03-sigform', component: D09s03SigformComponent},
+  {path: 'd09s04-sigform', component: D09s04SigformComponent},
+  {path: 'd09s05-sigform', component: D09s05SigformComponent},
+  {path: 'd09s06-sigform', component: D09s06SigformComponent},
+  {path: 'd09s07-sigform', component: D09s07SigformComponent},
+  {path: 'd09s08-sigform', component: D09s08SigformComponent},
+  {path: 'd09s09-sigform', component: D09s09SigformComponent},
+  {path: 'd09s10-sigform', component: D09s10SigformComponent},
+  {path: 'd09s11-sigform', component: D09s11SigformComponent},
+  {path: 'd09s12-sigform', component: D09s12SigformComponent},
+  {path: 'd10s01-sigform', component: D10s01SigformComponent},
+
 ];
